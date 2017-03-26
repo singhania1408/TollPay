a32be7730e6eea14fa4259f7044a0345510a58b0
Abhishek Singhania <abhisinghania14@gmail.com>
2017-03-26 21:17:28 +0530

    Payment Gateway Integration and GPS Recieve

---
+++ app/src/main/java/digiwizards/sih/com/tollpay/payment/PaymentResult.java
@@ -0,0 +1,94 @@
+package digiwizards.sih.com.tollpay.payment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+import digiwizards.sih.com.tollpay.R;
+
+public class PaymentResult extends AppCompatActivity implements PaymentResultListener{
+
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_result);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        startPayment();
+
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+
+    }
+
+    public void startPayment() {
+        /**
+         * Instantiate Checkout
+         */
+        Checkout checkout = new Checkout();
+
+        /**
+         * Set your logo here
+         */
+        checkout.setImage(android.R.drawable.ic_menu_more);
+
+        /**
+         * Reference to current activity
+         */
+        final Activity activity = this;
+
+        /**
+         * Pass your payment options to the Razorpay Checkout as a JSONObject
+         */
+        try {
+            JSONObject options = new JSONObject();
+
+            /**
+             * Merchant Name
+             * eg: Rentomojo || HasGeek etc.
+             */
+            options.put("name", "TollPay");
+
+            /**
+             * Description can be anything
+             * eg: Order #123123
+             *     Invoice Payment
+             *     etc.
+             */
+            options.put("description", "Order #123456");
+
+            options.put("currency", "INR");
+
+            /**
+             * Amount is always passed in PAISE
+             * Eg: "500" = Rs 5.00
+             */
+            options.put("amount", "50");
+            checkout.setFullScreenDisable(true);
+            checkout.open(activity, options);
+        } catch(Exception e) {
+            Log.e(TAG, "Error in starting Razorpay Checkout", e);
+        }
+    }
+}
---
+++ app/src/main/java/digiwizards/sih/com/tollpay/payment/Utils.java
@@ -0,0 +1,16 @@
+package digiwizards.sih.com.tollpay.payment;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.razorpay.Checkout;
+
+import org.json.JSONObject;
+
+/**
+ * Created by mrsinghania on 26/3/17.
+ */
+
+public class Utils {
+
+}
